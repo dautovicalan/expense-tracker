254e64bf2a57da7f4cb6509a9d67b8a4aaa1c816
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-21 09:23:03 +0100

    comments

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -4,9 +4,11 @@ import { useNavigate } from "react-router-dom";
 import TextBox from "./TextBox";
 import { useContext } from "react";
 import { DataContext } from "../Context/DataContext";
+import { Route } from "react-router-dom";
 
 const Overview = () => {
   const { data } = useContext(DataContext);
+  // TODO Look for outlet solution for nested routing
   return (
     <React.Fragment>
       <h2>Overview for December</h2>
