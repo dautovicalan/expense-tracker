574793730f609dadd242687b70e48d950a58eee1
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-23 16:19:15 +0100

    sorting logic

--- src/pages/DynamicPage.jsx
+++ src/pages/DynamicPage.jsx
@@ -17,23 +17,49 @@ const DynamicPage = () => {
   const [state, setState] = useState(details);
   const providedArray = useMemo(() => ({ state, setState }), [state, setState]);
 
+  const [isSortedDesc, setisSortedDesc] = useState(false);
+  const [isSortedAsc, setisSortedAsc] = useState(false);
+
+  const clickHandlerDesc = () => {
+    setisSortedDesc(true);
+    setisSortedAsc(false);
+  };
+  const clickHandlerAsc = () => {
+    setisSortedDesc(false);
+    setisSortedAsc(true);
+  };
+
   useEffect(() => {
-    setState(details);
-  }, [details]);
+    if (isSortedDesc) {
+      const sortedArr = [...details].sort((a, b) => b.value - a.value);
+      setState(sortedArr);
+    }
+    if (isSortedAsc) {
+      const sortedArr = [...details].sort((a, b) => a.value - b.value);
+      setState(sortedArr);
+    }
+    if (!isSortedAsc && !isSortedDesc) {
+      setState(details);
+    }
+  }, [details, isSortedDesc, isSortedAsc]);
 
   return (
     <div className={styles.details_container}>
       <h2>{type}</h2>
       <GoBackButton />
+      <button onClick={clickHandlerDesc}>Sort Desc</button>
+      <button onClick={clickHandlerAsc}>Sort Asc</button>
       {!isLoading &&
-        state.map((elemet, index) => {
+        state.map((element, index) => {
           return (
             <div key={index} className={styles.item_textbox}>
-              <h3>{elemet.name}</h3>
-              <h3>{elemet.value.toFixed(2)}</h3>
+              <h3>{element.name}</h3>
+              <h3>{element.value.toFixed(2)}</h3>
               <button
                 className={buttonStyle["button-34"]}
-                onClick={() => handleRemove(providedArray, index, data, typeId, setData)}
+                onClick={() =>
+                  handleRemove(providedArray, index, data, typeId, setData)
+                }
               >
                 REMOVE
               </button>
