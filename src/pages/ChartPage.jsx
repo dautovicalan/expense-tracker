380741c7f2b23f819f84fbc4e4576407c4957324
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-30 21:52:17 +0100

    dobar fix

--- src/pages/ChartPage.jsx
+++ src/pages/ChartPage.jsx
@@ -35,14 +35,18 @@ const ChartPage = () => {
           }),
         ],
         backgroundColor: [
-          myData.map((element) => {
-            return element.color;
-          }),
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
         ],
         borderColor: [
-          myData.map((element) => {
-            return element.color;
-          }),
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
         ],
         borderWidth: 1,
       },
@@ -61,7 +65,6 @@ const ChartPage = () => {
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
@@ -69,7 +72,6 @@ const ChartPage = () => {
           "rgba(255, 206, 86, 1)",
           "rgba(75, 192, 192, 1)",
           "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
         ],
         borderWidth: 1,
       },
