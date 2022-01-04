bc463183ac1b9cc16ff78b0790afb08d6350954e
RealMeKaNiK <dautovic2000@gmail.com>
2022-01-04 21:03:41 +0100

    klipi board

--- src/testing/testing.js
+++ src/testing/testing.js
@@ -2,6 +2,7 @@ const getingSwapi = async () => {
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
     console.log(data);
+    navigator.clipboard.writeText("ja sam peder");
 }
 
 getingSwapi();
